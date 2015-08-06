e9aaf991367666ca1fc1d592c81c6b25f5bfdf17
Thomas Smoker <tomsmoker@gmail.com>
2015-08-06 21:15:47 +0800

    Initialised the model
    - Added ten coffee shops in the Perth CBD that i like

---
+++ js/model.js
@@ -0,0 +1,63 @@
+var coffeeShops = [
+  {
+  	name: "Lowdown",
+  	address: "16a Cloisters Arcade, Parmelia House, 865 Hay St, Perth WA 6000",
+  	lat: -31.9530869,
+  	lng: 115.8533204
+  },
+  {
+  	name: "Small Print",
+  	address: "125 St Georges Terrace, Perth WA 6000",
+  	lat: -31.954965,
+  	lng: 115.854671
+  },
+  {
+  	name: "Standing Room Only",
+  	address: "Shop 7, Piccadilly Arcade,Perth WA 6000",
+  	lat: -31.9528473,
+  	lng: 115.8550868
+  },
+  {
+  	name: "Velvet Espresso",
+  	address: "172 St Georges Terrace, Perth WA 6000",
+  	lat: -31.9537116,
+  	lng: 115.8543291
+  },
+  {
+  	name: "Ristretto Espresso",
+  	address: "160 St Georges Terrace, Perth WA 6000",
+  	lat: -31.954074,
+  	lng: 115.855036
+  },
+  {
+  	name: "Driphouse",
+  	address: "772A Hay St, Perth WA 6000",
+  	lat: -31.95314,
+  	lng: 115.8562336
+  },
+  {
+  	name: "Moana Coffee",
+  	address: "618 Hay St, Perth WA 6000",
+  	lat: -31.954314,
+  	lng: 115.8600435
+  },
+  {
+  	name: "Willie Wagtail",
+  	address: "131 William Street, Perth WA 6000",
+  	lat: -31.951648,
+  	lng: 115.8578971
+  },
+  {
+  	name: "Voltage Espresso",
+  	address: "12/50 St Georges Terrace, Perth WA 6000",
+  	lat: -31.955158,
+  	lng: 115.859048
+  },
+  {
+  	name: 'Cheeky Sparrow',
+  	address: "317 Murray St, Perth WA 6000",
+  	lat: -31.9523251,
+  	lng: 115.8560175
+  }
+];
+
